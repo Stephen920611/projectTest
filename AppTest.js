6c2cbe475bc7591fe4ca8ff1ec15c54b5a962bdb
stephen <13105208266@163.com>
2019-12-17 11:47:14 +0800

    第一次提交

---
+++ AppTest.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class FlexDimensionsBasics extends Component {
+
+    render() {
+        return (
+            // 试试去掉父View中的`flex: 1`。
+            // 则父View不再具有尺寸，因此子组件也无法再撑开。
+            // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+            <View style={{height: 300}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+}
